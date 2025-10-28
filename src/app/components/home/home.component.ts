6333c9f1c560abf9d61ed8cd8eb8a118983364f6
ankitsingh-pythrust <162984477+ankitsingh-pythrust@users.noreply.github.com>
2025-10-28 17:50:57 +0530

    Add/update src/app/components/home/home.component.ts

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
+import { MatDividerModule } from '@angular/material/divider';
+
+@Component ({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, MatCardModule, RouterModule, MatButtonModule, MatDividerModule],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']}
+)
+export class HomeComponent {}
